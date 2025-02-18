c11e33c1208fd8f59e51401b39a7a4280b8cd411
ktflk <adol.fik.00@mail.ru>
2025-02-18 13:50:30 +0500

    пщ87л

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 from .models import Client
 
-admin.site.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'address', 'date_of_birth', 'created_at', 'updated_at')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number', 'address')
+
+admin.site.register(Client, ClientAdmin)
 
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,7 +5,10 @@ class Client(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True)
-
+    adress = models.CharField(max_length=50)
+    date_of_birth = models.DateField(max_length=10)
+    created_at = models.DateTimeField(max_length=20)
+    updated_at = models.DateTimeField(max_length=20)
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
