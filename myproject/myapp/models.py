d9f6e14a9c23d7631ebd6c7b435bf8a978867035
ktflk <adol.fik.00@mail.ru>
2025-02-25 19:52:34 +0500

    6oy7t

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,12 +5,12 @@ class Client(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True)
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, default="")
     date_of_birth = models.DateField(max_length=20)
-    created_at = models.DateTimeField(max_length=20)
-    updated_at = models.DateTimeField(max_length=20)
-    def __str__(self):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def str(self):
         return f'{self.first_name} {self.last_name}'
 
 
-# Create your models here.
+# Create your models here.ь
